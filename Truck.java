f2ad7c79fd219b1eb6530d7d9b5b8cc7a934207f
Payson Parker <90354190+paysonjparker@users.noreply.github.com>
2021-10-13 20:16:58 -0700

    Truck class
    Extends the car class.

---
+++ Truck.java
@@ -0,0 +1,26 @@
+
+/**
+ * This is the Truck class, which extends the car class. I overrode the 
+ * honk() method from the car class to give the Truck its own special behavior.
+ * @author paysonparker
+ * 10/13/2021
+ */
+public class Truck extends Car {
+
+	/**
+	 * Creates a new Truck object.
+	 * @param carName The car's name.
+	 * @param price The car's price.
+	 */
+	public Truck(String carName, double price) {
+		super(carName, price);
+	}
+	
+	/**
+	 * Makes the truck honk its horn.
+	 */
+	public void honk() {
+		System.out.println(getCarName() + " honked: HOOOOONNNNNKKK!");
+	}
+
+}
